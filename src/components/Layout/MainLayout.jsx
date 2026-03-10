6e6c712f2c8d183cc5f39f2076c9bbba8a9bd92f
Your Name <bensonksunny@gmail.com>
2026-03-10 17:15:52 +0530

    Main content width 3/4 on desktop; full width on mobile

--- src/components/Layout/MainLayout.jsx
+++ src/components/Layout/MainLayout.jsx
@@ -6,7 +6,9 @@ export default function MainLayout() {
     <div className="min-h-screen bg-slate-50">
       <Sidebar />
       <main className="lg:ml-64 min-h-screen p-4 pt-14 lg:pt-6 lg:p-8">
-        <Outlet />
+        <div className="w-full lg:w-3/4 max-w-full mx-auto">
+          <Outlet />
+        </div>
       </main>
     </div>
   )
